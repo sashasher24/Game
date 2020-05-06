d17d1c88e80a8a753c4569a99e5fe6064d215b56
Oleksandra Sherstiuk <osherstiuk@e2r11p17.unit.ua>
2020-05-06 13:04:36 +0300

    teper dog letaet

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 #include "SDL_image.h"
 #include <stdio.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 #define MAX_BULLETS 1000
 
-typedef struct
-{
+typedef struct {
   float x, y, dy;
   short life;
   char *name;
@@ -15,32 +17,27 @@ typedef struct
   SDL_Texture *sheetTexture;
 } Man;
 
-typedef struct
-{
+typedef struct {
   float x, y, dy;
 } Bullet;
 
 SDL_Texture *bulletTexture;
 SDL_Texture *backgroundTexture;
 Bullet *bullets[MAX_BULLETS] = { NULL };
-Man enemy;
+// Man enemy;
 
 int globalTime = 0;
 
-void addBullet(float x, float y, float dy)
-{
+void addBullet(float x, float y, float dy) {
   int found = -1;
-  for(int i = 0; i < MAX_BULLETS; i++)
-  {
-    if(bullets[i] == NULL)
-    {
+  for (int i = 0; i < MAX_BULLETS; i++) {
+    if (bullets[i] == NULL) {
       found = i;
       break;
     }
   }
 
-  if(found >= 0)
-  {
+  if (found >= 0) {
     int i = found;
     bullets[i] = malloc(sizeof(Bullet));
     bullets[i]->x = x;
@@ -49,28 +46,22 @@ void addBullet(float x, float y, float dy)
   }
 }
 
-void removeBullet(int i)
-{
-  if(bullets[i])
-  {
+void removeBullet(int i) {
+  if (bullets[i]) {
     free(bullets[i]);
     bullets[i] = NULL;
   }
 }
 
-int processEvents(SDL_Window *window, Man *man)
-{
+int processEvents(SDL_Window *window, Man *man, Man *dog) {
   SDL_Event event;
   int done = 0;
 
-  while(SDL_PollEvent(&event))
-  {
-    switch(event.type)
-    {
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
       case SDL_WINDOWEVENT_CLOSE:
       {
-        if(window)
-        {
+        if (window) {
           SDL_DestroyWindow(window);
           window = NULL;
           done = 1;
@@ -79,8 +70,7 @@ int processEvents(SDL_Window *window, Man *man)
       break;
       case SDL_KEYDOWN:
       {
-        switch(event.key.keysym.sym)
-        {
+        switch (event.key.keysym.sym) {
           case SDLK_ESCAPE:
             done = 1;
           break;
@@ -96,51 +86,73 @@ int processEvents(SDL_Window *window, Man *man)
   
   const Uint8 *state = SDL_GetKeyboardState(NULL);
     
-    if(man->x >= -40){
+    if (state[SDL_SCANCODE_LEFT]) {
+      dog->x -= 3;
+      dog->walking = 1; 
+      if (dog->x < 0) dog->x = WINDOW_WIDTH - 40;
+    }
+    if (state[SDL_SCANCODE_RIGHT]) {
+      dog->x += 3;
+      dog->walking = 1;
+      if (dog->x > WINDOW_WIDTH) dog->x = 0;
+    }
+    if (state[SDL_SCANCODE_UP]) {
+      if (dog->y - 3 < 300){
+          dog->y = 60;
+      }
+      else {
+          dog->y -= 3;
+          dog->walking = 1;
+      }
+    }
+    if (state[SDL_SCANCODE_DOWN]) {
+      if (dog->y + 3 + 60 > WINDOW_HEIGHT) {
+        dog->y = WINDOW_HEIGHT - 60;
+      }
+      else {
+        dog->y += 3;
+        dog->walking = 1;
+      }
+    }
+
+    if (man->x >= -40) {
         man->x += 1;
         man->walking = 1;
-        if(man->x == 320){
+        if (man->x == 320) {
             man->x = -40;
         }
     }
    
-     if(state[SDL_SCANCODE_SPACE])
-    {
-      if(globalTime % 6 == 0)
-      {
-        if(man->currentSprite == 4)
+     if (state[SDL_SCANCODE_SPACE]) {
+      if (globalTime % 6 == 0) {
+        if (man->currentSprite == 4)
           man->currentSprite = 5;
-        else{
+        else {
           man->currentSprite = 4;
         }
-          
-        
           addBullet(man->x, man-> y+70, 3);
-        
       }
-  
       man->shooting = 1;
     }
-    else
-    {
+    else {
       man->currentSprite = 4;
       man->shooting = 0;
     }
   
   
-if(state[SDL_SCANCODE_UP] && !man->dy)
-  {
-    man->dy = -8;
-  }
-  if(state[SDL_SCANCODE_DOWN])
-  {
-    //man->y += 10;
-  }
+// if(state[SDL_SCANCODE_UP] && !man->dy)
+//   {
+//     man->dy = -8;
+//   }
+//   if(state[SDL_SCANCODE_DOWN])
+//   {
+//     //man->y += 10;
+//   }
   
   return done;
 }
 
-void doRender(SDL_Renderer *renderer, Man *man)
+void doRender(SDL_Renderer *renderer, Man *man, Man *dog)
 {
   //set the drawing color to blue
   SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
@@ -155,23 +167,20 @@ void doRender(SDL_Renderer *renderer, Man *man)
   SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
 
   //warrior
-  if(man->visible)
-  {
+  if (man->visible) {
     SDL_Rect srcRect = { 40*man->currentSprite, 0, 40, 50 };
     SDL_Rect rect = { man->x, man->y, 40, 50 };
     SDL_RenderCopyEx(renderer, man->sheetTexture, &srcRect, &rect, 0, NULL, man->facingLeft);
   }
 
   //enemy
-  if(enemy.visible)
-  {
-    SDL_Rect eSrcRect = { 40*enemy.currentSprite, 0, 40, 50 };
-    SDL_Rect eRect = { enemy.x, enemy.y, 40, 50 };
-    SDL_RenderCopyEx(renderer, enemy.sheetTexture, &eSrcRect, &eRect, 0, NULL, enemy.facingLeft);
+  if (dog->visible) {
+     SDL_Rect eSrcRect = { 40, 0, 40, 60 };
+   // SDL_Rect eSrcRect = { 40*enemy.currentSprite, 0, 40, 50 };  
+    SDL_Rect eRect = { dog->x, dog->y, 40, 60 };  
+    SDL_RenderCopyEx(renderer, dog->sheetTexture, &eSrcRect, &eRect, 0, NULL, dog->facingLeft);
   }
-
-  for(int i = 0; i < MAX_BULLETS; i++) if(bullets[i])
-  {
+ for (int i = 0; i < MAX_BULLETS; i++) if(bullets[i]) {
     SDL_Rect rect = { bullets[i]->x, bullets[i]->y, 8, 8 };
     SDL_RenderCopy(renderer, bulletTexture, NULL, &rect);
   }
@@ -180,23 +189,23 @@ void doRender(SDL_Renderer *renderer, Man *man)
   SDL_RenderPresent(renderer);
 }
 
-void updateLogic()
-{
+void updateLogic(Man *dog) {
 
-for(int i = 0; i < MAX_BULLETS; i++) if(bullets[i])
-  {
-    bullets[i]->y += bullets[i]->dy;
-    
-    //simple coll. detection
+  for(int i = 0; i < MAX_BULLETS; i++) {
+    if(bullets[i]) {
+      bullets[i]->y += bullets[i]->dy;
       
-    if(bullets[i]->x > enemy.x && bullets[i]->x < enemy.x+40 &&
-       bullets[i]->y > enemy.y && bullets[i]->y < enemy.y+50)
-    {
-      enemy.alive = 0;           // вот тут добавляем очки а не вот это гавно
+      //simple coll. detection
+        
+      if(bullets[i]->x > dog->x && bullets[i]->x < dog->x+40 &&
+        bullets[i]->y > dog->y && bullets[i]->y < dog->y+50)
+      {
+        dog->alive = 0;           // вот тут добавляем очки а не вот это гавно
+      }
+      
+      if(bullets[i]->x < -1000 || bullets[i]->x > 1000)
+        removeBullet(i);
     }
-    
-    if(bullets[i]->x < -1000 || bullets[i]->x > 1000)
-      removeBullet(i);
   }
     
   globalTime++;
@@ -217,24 +226,25 @@ int main()
   man.visible = 1;
   man.facingLeft = 0;
   
-  enemy.x = 250;
-  enemy.y = 60;
-  enemy.currentSprite = 4;
-  enemy.facingLeft = 1;
-  enemy.alive = 1;
-  enemy.visible = 1;
+  Man dog;
+  dog.x = 350;
+  dog.y = 400;
+  dog.currentSprite = 4;
+  dog.facingLeft = 1;
+  dog.alive = 1;
+  dog.visible = 1;
   
   //Create an application window with the following settings:
   window = SDL_CreateWindow("Game Window",                     // window title
                             SDL_WINDOWPOS_UNDEFINED,           // initial x position
                             SDL_WINDOWPOS_UNDEFINED,           // initial y position
-                            640,                               // width, in pixels
-                            480,                               // height, in pixels
+                            WINDOW_WIDTH,                               // width, in pixels
+                            WINDOW_HEIGHT,                               // height, in pixels
                             0                                  // flags
                             );
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-  SDL_RenderSetLogicalSize(renderer, 320, 240);
+  //SDL_RenderSetLogicalSize(renderer, 320, 240);
 
   
   SDL_Surface *sheet = IMG_Load("sheet.png");
@@ -251,11 +261,11 @@ int main()
   sheet = IMG_Load("badman_sheet.png");
   if(!sheet)
   {
-    printf("Cannot find sheet\n");
+    printf("Cannot find dog sheet\n");
     return 1;
   }
   
-  enemy.sheetTexture = SDL_CreateTextureFromSurface(renderer, sheet);
+  dog.sheetTexture = SDL_CreateTextureFromSurface(renderer, sheet);
   SDL_FreeSurface(sheet);
 
   //load the bg
@@ -289,13 +299,13 @@ int main()
   while(!done)
   {
     //Check for events
-    done = processEvents(window, &man);
+    done = processEvents(window, &man, &dog);
     
     //Update logic
-    updateLogic();
+    updateLogic(&dog);
     
     //Render display
-    doRender(renderer, &man);
+    doRender(renderer, &man, &dog);
     
     //don't burn up the CPU
     SDL_Delay(10);
@@ -308,7 +318,7 @@ int main()
   SDL_DestroyTexture(man.sheetTexture);
   SDL_DestroyTexture(backgroundTexture);
   SDL_DestroyTexture(bulletTexture);
-  SDL_DestroyTexture(enemy.sheetTexture);
+  SDL_DestroyTexture(dog.sheetTexture);
   
   for(int i = 0; i < MAX_BULLETS; i++)
     removeBullet(i);
@@ -317,6 +327,3 @@ int main()
   SDL_Quit();
   return 0;
 }
-
-
-
